108dcdc19a776cdab1e121d4009f82a7fdbcf4c5
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-06-21 10:35:02 -0700

    add in progress scripts -messy

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//takes use input
+
+//Pizza constructor
+function Pizza(size, toppings, price) {
+  this.size = size;
+  this.toppings1 = toppings1;
+  this.toppings2 = toppings2
+  this.price = price;
+};
+//Pizza modifiers
+var size = $("input#size-select").val();
+console.log(size)
+
+Pizza.prototype.price = function () {
+
+  console.log(price)
+};
+//return Price
