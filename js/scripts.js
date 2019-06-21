de99dc41d59eba6c66522858f7cfee5d452d11d4
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-06-21 16:27:27 -0700

    add little of value, hit a brain wall and spiralled, will come back to this tomorrow

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,14 @@
-// ---------Backend kitchen logic----------//
-function Kitchen() {
-    this.order = []
-    // this.item = 0
-    // console.log(order)
-}
-Kitchen.prototype.addPizza = function(Pizza) {
-  // thisPizza.id = this.assignId();
-  this.order.push(Pizza);
-}
-
-// ----------Backend Pizza logic---------
-function Pizza(size, topping1, topping2) {
-  this.size = size;
-  this.firstTopping = topping1;
-  this.secondTopping = topping2;
-  // this.cost = price;
-  console.log(topping1)
-}
-
-// Kitchen.prototype.assignId = function() {
-//   this.currentId += 1;
-//   return this.currentId;
-// }
-
-
 //---------Front end customer logic--------------
-  var kitchen = new Kitchen();
+var kitchen = new Kitchen();
 
-  function serveDatVirtualPizza(pizzaWorthShowing) {
-    var thePizzas = $("ul#order");
-    var htmlforPizzaInfo = "";
-    pizzaWorthShowing.order.forEach(function(Pizza) {
-      htmlForPizzaInfo += "<li id="  "Pizza" "</li>";
-    });
-    thePizzas.html(htmlForPizzaInfo);
-  };
+function serveDatVirtualPizza(pizzaWorthShowing) {
+  var thePizzas = $("ul#order");
+  var htmlforPizzaInfo = "";
+  pizzaWorthShowing.order.forEach(function(Pizza) {
+    htmlForPizzaInfo `<li>"inputtedSize" "inputtedTopping1" "inputtedTopping2"</li>`;
+  });
+  thePizzas.html(htmlForPizzaInfo);
+};
 
 
 $(document).ready(function() {
@@ -44,8 +18,8 @@ $(document).ready(function() {
     var inputtedTopping1 = $("select#topping1-select").val();
     var inputtedTopping2 = $("select#topping2-select").val();
     var thisPizza = new Pizza("inputtedSize", "inputtedTopping1", "inputtedTopping2");
-    Kitchen.addPizza(thisPizza);
-    serveDatVirtualPizza(order);
+    kitchen.addPizza(Pizza);
+    serveDatVirtualPizza(Pizza);
     console.log(inputtedSize);
     console.log(inputtedTopping1);
     console.log(inputtedTopping2);
@@ -53,3 +27,19 @@ $(document).ready(function() {
     console.log(Pizza)
   });
 });
+// ---------Backend kitchen logic----------//
+function Kitchen() {
+    this.order = [],
+    this.item = 0
+}
+Kitchen.prototype.addPizza = function(Pizza) {
+  thisPizza.id = this.assignId();
+  this.order.push(Pizza);
+}
+
+// ----------Backend Pizza logic---------
+function Pizza(size, topping1, topping2) {
+  this.size = size;
+  this.firstTopping = topping1;
+  this.secondTopping = topping2;
+}
