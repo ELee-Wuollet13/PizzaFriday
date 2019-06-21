7a9d205c164f757bced35c2a30e22ea0908c3922
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-06-21 12:32:16 -0700

    add progress towards working constructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,35 @@
 function YourOrder() {
   this.order = [],
   this.item = 0
+  console.log()
 }
 
 $(document).ready(function() {
   $("form#size-form").submit(function(event) {
     event.preventDefault();
-    var size = $("select#size-select").val();
-    var topping1 = $("select#topping1-select").val();
-    var topping2 = $("select#topping2-select").val();
+    // var size = $("select#size-select").val();
+    // var topping1 = $("select#topping1-select").val();
+    // var topping2 = $("select#topping2-select").val();
     // var Pizza = new Pizza(size, topping1, topping2);
 
+    // console.log(size)
+    // console.log(topping1)
+    // console.log(topping2)
 
-    console.log(size)
-    console.log(topping1)
-    console.log(topping2)
 
-  })
-  })
-//Pizza constructor
-function Pizza(size, toppings, price) {
-  this.size = size;
-  this.toppings1 = toppings1;
-  this.toppings2 = toppings2
-  this.price = price;
+  function Pizza(pizzaSize, firstTopping, secondTopping, cost) {
+    this.pizzaSize = $("select#size-select").val();
+    this.firstTopping = $("select#topping1-select").val();
+    this.secondTopping = $("select#topping2-select").val();
+    this.cost = price;
+
+var thisPizza = new Pizza("size", "topping1", "topping2") {
+  .push(YourOrder)
 };
-//Pizza modifiers
 
-//
-// Pizza.prototype.price = function () {
-//
-//   console.log(price)
-// };
-//return Price
+console.log(Pizza)
+
+//Pizza constructor
+};
+});
+});
