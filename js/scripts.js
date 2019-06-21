b03297da2ecf4bfacbed9f6e98406090cae89e6a
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-06-21 11:42:32 -0700

    define 'size' 'topping1' 'topping2' variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 //takes use input
+function YourOrder() {
+  this.order = [],
+  this.item = 0
+}
 
+$(document).ready(function() {
+  $("form#size-form").submit(function(event) {
+    event.preventDefault();
+    var size = $("select#size-select").val();
+    var topping1 = $("select#topping1-select").val();
+    var topping2 = $("select#topping2-select").val();
+    // var Pizza = new Pizza(size, topping1, topping2);
+
+
+    console.log(size)
+    console.log(topping1)
+    console.log(topping2)
+
+  })
+  })
 //Pizza constructor
 function Pizza(size, toppings, price) {
   this.size = size;
@@ -8,11 +27,10 @@ function Pizza(size, toppings, price) {
   this.price = price;
 };
 //Pizza modifiers
-var size = $("input#size-select").val();
-console.log(size)
 
-Pizza.prototype.price = function () {
-
-  console.log(price)
-};
+//
+// Pizza.prototype.price = function () {
+//
+//   console.log(price)
+// };
 //return Price
