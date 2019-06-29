48f9af7b685762b7e10959a25b5211543992fcd6
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-06-29 01:48:52 -0700

    complete js functionality. HTML and CSS to be completed later

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,24 @@
-//---------Front end customer logic--------------
-var kitchen = new Kitchen();
+function Pizza(size, firstTopping, secondTopping) {
+  this.size = size;
+  this.firstTopping = firstTopping;
+  this.secondTopping = secondTopping;
+  console.log(size);
+  console.log(firstTopping);
+  console.log(secondTopping);
 
-function serveDatVirtualPizza(pizzaWorthShowing) {
-  var thePizzas = $("ul#order");
-  var htmlforPizzaInfo = "";
-  pizzaWorthShowing.order.forEach(function(Pizza) {
-    htmlForPizzaInfo `<li>"inputtedSize" "inputtedTopping1" "inputtedTopping2"</li>`;
-  });
-  thePizzas.html(htmlForPizzaInfo);
-};
-
+}
 
 $(document).ready(function() {
   $("form#order-form").submit(function(event) {
     event.preventDefault();
-    var inputtedSize = $("select#size-select").val();
-    var inputtedTopping1 = $("select#topping1-select").val();
-    var inputtedTopping2 = $("select#topping2-select").val();
-    var thisPizza = new Pizza("inputtedSize", "inputtedTopping1", "inputtedTopping2");
-    kitchen.addPizza(Pizza);
-    serveDatVirtualPizza(Pizza);
-    console.log(inputtedSize);
-    console.log(inputtedTopping1);
-    console.log(inputtedTopping2);
-    // .push(YourOrder)
-    console.log(Pizza)
+    var size = $("select#size-select").val();
+    var firstTopping = $("select#topping1-select").val();
+    var secondTopping = $("select#topping2-select").val();
+    var thisPizza = new Pizza(size, firstTopping, secondTopping);
+    thisPizza.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+    console.log(size);
+    console.log(thisPizza);
   });
 });
-// ---------Backend kitchen logic----------//
-function Kitchen() {
-    this.order = [],
-    this.item = 0
-}
-Kitchen.prototype.addPizza = function(Pizza) {
-  thisPizza.id = this.assignId();
-  this.order.push(Pizza);
-}
-
-// ----------Backend Pizza logic---------
-function Pizza(size, topping1, topping2) {
-  this.size = size;
-  this.firstTopping = topping1;
-  this.secondTopping = topping2;
-}
