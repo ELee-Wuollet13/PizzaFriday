489df52d6f30360f7270d57beb3ef5b50936face
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-06-29 19:07:47 -0700

    display pizza on main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-function Pizza(size, firstTopping, secondTopping) {
+function pizza(size, firstTopping, secondTopping) {
   this.size = size;
   this.firstTopping = firstTopping;
   this.secondTopping = secondTopping;
   console.log(size);
   console.log(firstTopping);
   console.log(secondTopping);
-
 }
 
 $(document).ready(function() {
@@ -14,11 +13,15 @@ $(document).ready(function() {
     var size = $("select#size-select").val();
     var firstTopping = $("select#topping1-select").val();
     var secondTopping = $("select#topping2-select").val();
-    var thisPizza = new Pizza(size, firstTopping, secondTopping);
-    thisPizza.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    var thisPizza = new pizza(size, firstTopping, secondTopping);
+    var result = [size, + "_" + firstTopping, + "_" + secondTopping];
+    var joined = result.join("");
+    $(".output").text(joined);
+    $(".thisPizza").text(size);
+    $(".thisPizza").text(firstTopping);
+    $(".thisPizza").text(secondTopping);
+
     console.log(size);
     console.log(thisPizza);
   });
-});
+  });
